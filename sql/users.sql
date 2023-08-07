a696543e87f904ed93b296e5be8ffa351d9657a8
jajuoMarketCoder <jajuo3141@gmail.com>
2023-08-07 21:21:23 +0200

    changes password_hash db type
    - password_hash in users table changed to CHAR

--- sql/users.sql
+++ sql/users.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(255) NOT NULL PRIMARY KEY,
-    password_hash BINARY(60) NOT NULL,
+    password_hash CHAR(60) NOT NULL,
     user_description TEXT
 )
